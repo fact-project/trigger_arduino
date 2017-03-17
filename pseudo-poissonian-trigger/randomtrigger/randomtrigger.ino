4ad2870e686f0c497118ef5095a258a3705fd324
Dominik Neise <neised@phys.ethz.ch>
2017-03-17 07:46:20 +0100

    rename `output` to `trigger_pin`, output could be anything.
    Also, it is common to store the pin number in a const.

--- pseudo-poissonian-trigger/randomtrigger/randomtrigger.ino
+++ pseudo-poissonian-trigger/randomtrigger/randomtrigger.ino
@@ -1,8 +1,14 @@
+/* Pseudo-poissonian trigger for FACT
+ *
+ * If you want have a smaller mean rate than 200 Events/s, give tau in milliseconds
+ * and use delay(delta_ts[i]) instead of delayMicroseconds(delta_ts[i]).
+ * The arduino delayMicroseconds function only works correctly with delays < 16383.
+ */
+
 #include "tinymt64.h"
 
 tinymt64_t tinymt;
-unsigned short output = 2;
-
+const unsigned short trigger_pin = 2;
 const unsigned short buffer_size = 500;
 unsigned short delta_ts[buffer_size];
 double tau = 5000;  // mean delta_t in micro seconds
@@ -14,9 +20,9 @@ double exponential_random_number(double tau){
 }
 
 void setup() {
-  pinMode(output, OUTPUT);
-  digitalWrite(output, HIGH);
-  tinymt64_init(&tinymt, analogRead(A0)); 
+  pinMode(trigger_pin, OUTPUT);
+  digitalWrite(trigger_pin, HIGH);
+  tinymt64_init(&tinymt, analogRead(A0));
   Serial.begin(9600);
   while(!Serial){
     delay(1);
@@ -45,13 +51,9 @@ void loop() {
     }
 
     for(int i = 0; i < buffer_size; i++){
-      if (delta_ts[i] > 15000){
-        delay(delta_ts[i] / 1000);
-      } else {
-        delayMicroseconds(delta_ts[i]);
-      }
-      digitalWrite(output, LOW);
-      digitalWrite(output, HIGH);
+      delayMicroseconds(delta_ts[i]);
+      digitalWrite(trigger_pin, LOW);
+      digitalWrite(trigger_pin, HIGH);
     }
   }
 }
